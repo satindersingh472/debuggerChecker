36a0cad8f9d53803ccf88710c09e81d1da50924f
satinder <satindersingh472@gmail.com>
2022-07-07 19:31:51 -0600

    checked how debugger works

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let user_name = "satinder";
+let user_age = 27;
+let is_member = true;
+if ((user_name === "admin") || (user_age >= 30 && is_member === true)){
+    console.log("The user is admin  either 30 or above and for sure is the member");
+} else {
+    console.log("user is not admin");
+}
